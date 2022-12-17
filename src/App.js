94f21c0cf580e5f29385ce0bbc7daca7c294290e
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2022-12-17 18:26:28 +0600

    nav home notfound create

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title={"GitHub Finder"}></Navbar>
+        <main className="container mx-auto pxx-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/about" element={<About></About>}></Route>
+            <Route path="/notfound" element={<NotFound></NotFound>}></Route>
+            <Route path="/*" element={<NotFound></NotFound>}></Route>
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        This App is Built with React.User can search Github profiles and see
+        profile details
+      </p>
+      <p className="text-lg text-gray-400"> Version : 1.0.0</p>
+    </div>
+  );
+}
+
+export default About;
